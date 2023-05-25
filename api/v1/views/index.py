8db299b330c2813b2695156cb066dd88f9ff6801
GideonAmhaG <gideonamha@gmail.com>
2023-05-26 00:22:15 +0300

    1st commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 """module for index"""
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import Flask, Blueprint, jsonify
+from models import storage
+
+
+hbnb_objs = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
 
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     """returns a JSON: "status": OK"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """Create an endpoint that retrieves the number of each objects by type"""
+    return_dict = {}
+    for key, value in hbnb_objs.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
