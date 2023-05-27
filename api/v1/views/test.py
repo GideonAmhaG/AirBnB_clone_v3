4de1c6f47d05710819baeae83c9aabc7413410b5
GideonAmhaG <gideonamha@gmail.com>
2023-05-27 22:02:58 +0300

    test

--- api/v1/views/test.py
+++ api/v1/views/test.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""module for api"""
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+if __name__ == "__main__":
+    pass
 
+
+hbnb_models = {
+	"Amenity": "amenities",
+	"City": "cities",
+	"Place": "places",
+	"Review": "reviews",
+	"State": "states",
+	"User": "users"
+}
 
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
-    """hbnbStatus"""
+    """returns a JSON: "status": OK"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def hbnbStats():
-    """hbnbStats"""
+    """Create an endpoint that retrieves the number of each objects by type"""
     return_dict = {}
-    for key, value in hbnbText.items():
+    for key, value in hbnb_models.items():
         return_dict[key] = storage.count(value)
     return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
