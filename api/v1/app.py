1ab9cbb7cd640f062a0731e17ab0103cf1ee5073
GideonAmhaG <gideonamha@gmail.com>
2023-05-27 23:22:21 +0300

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """module for app"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint
-from models import storage
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 from os import getenv
+from models import storage
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown(exception):
     """declare a method to handle @app.teardown_appcontext"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """create a handler for 404 errors that returns a JSON-formatted 404
+    status code response"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
